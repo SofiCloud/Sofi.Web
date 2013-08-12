77fd27b72f3ec099dc67e298756d9430d6c61e7f
Denny Wu <denny@inforsys.co.id>
2013-08-12 13:59:29 +0700

    test form sales order

---
+++ src/web/Sofi.Web/Scripts/app/Sales/SalesOrder/Create.js
@@ -0,0 +1,38 @@
+﻿Ext.define("Sofi.Sales.SalesOrder.Create", {
+    constructor: function () {
+        Ext.require(['*']);
+
+        var viewport = Ext.create('Ext.Viewport', {
+            id: 'form-sales-order',
+            layout: 'border',
+            items: [
+            {
+                region: 'east',
+                title: 'Toolbar',
+                dockedItems: [{
+                    dock: 'top',
+                    xtype: 'toolbar',
+                    items: ['->', {
+                        xtype: 'button',
+                        text: 'test',
+                        tooltip: 'Test Button',
+                        handler: function () {
+                            Ext.Msg.alert("HAHAHA", "HAHAHA");
+                        }
+                    }]
+                }],
+                animCollapse: true,
+                collapsible: true,
+                split: true,
+                width: 150, // give east and west regions a width
+                minSize: 135,
+                maxSize: 150
+            },
+            {
+                region: 'center',
+                title: 'Sales Order',
+                border: true
+            }]
+        });
+    }
+});
---
+++ src/web/Sofi.Web/Scripts/tests/extjs/FormSalesOrder.js
@@ -0,0 +1,15 @@
+﻿/*!
+* Ext JS Library 4.0
+* Copyright(c) 2006-2011 Sencha Inc.
+* licensing@sencha.com
+* http://www.sencha.com/license
+*/
+Ext.Loader.setPath({
+    'Sofi': '/Scripts/app'
+});
+
+Ext.require('Sofi.Sales.SalesOrder.Create');
+
+Ext.onReady(function () {
+    new Sofi.Sales.SalesOrder.Create();
+});
